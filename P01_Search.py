2aeffa4bb98df05658227f5b2015d7d393045286
Harihararam Sriram <harisriram2002@gmail.com>
2024-07-01 23:17:08 +0530

    setup script for searching and generating output

---
+++ P01_Search.py
@@ -0,0 +1,48 @@
+import sqlite3
+from typing import Dict, List, Tuple
+
+
+class Student:
+    name: str
+    marks: int
+
+    def __init__(self, name: str, marks: int) -> None:
+        self.name = name
+        self.marks = marks
+        pass
+
+
+def search(name: str) -> List[Student]:
+    with sqlite3.connect("./assignment.db") as conn:
+        curr = conn.cursor()
+        rows: List[Tuple[str, int]] = curr.execute(
+            f"SELECT * FROM students WHERE name LIKE ?", (f'%{name}%',)).fetchall()
+
+        results: List[Student] = [
+            Student(name=row[0], marks=row[1]) for row in rows]
+
+        return results
+
+
+if __name__ == "__main__":
+    name = ''
+    while True:
+        name = str(input('Enter the name of the student: '))
+        if (len(name) > 0):
+            break
+        print("Please enter a search string\n")
+
+    req_students = search(name)
+
+    if (len(req_students) == 0):
+        print("Oops! No students present that match the search text")
+
+    else:
+        print('Name\t\t|Marks')
+        for req_student in req_students:
+            print(req_student.name, req_student.marks, sep="\t\t|")
+        print("=====\n")
+
+        total_score = sum([student.marks for student in req_students])
+        average_score = round(total_score/len(req_students), 2)
+        print(f"Total Score: {total_score}", f"Average Score: {average_score}")
